f84ca5fb93eec08bf5a4eb56a3bb95d2b1580cbd
ZinYan <zinwaiyan2226@gmail.com>
2025-12-21 21:16:28 +0900

    [feat] Admin + Database Manipulation

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Zin',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2025'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
+    posts = Post.objects.all()
     context = {'posts':posts}
-    return render(request,'home.html',context)
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,"about.html",{"title" : "About"})
+    return render(request,"blog/about.html",{"title" : "About"})
